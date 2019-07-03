6814c06ad99e7700edf206741d3c9330bf90ef4e
Olena_Oliinyk <elenikqa@ukr.net>
2019-07-03 12:49:35 +0300

    edge webdriver initialized

--- src/test/java/JavaMentor/parentTest/ParentTest.java
+++ src/test/java/JavaMentor/parentTest/ParentTest.java
@@ -1,11 +1,10 @@
 package JavaMentor.parentTest;
 
 import org.apache.log4j.Logger;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import pages.MainUrl;
 
 import java.io.File;
@@ -16,7 +15,9 @@ import static org.hamcrest.CoreMatchers.is;
 public class ParentTest {
     public MainUrl mainUrl;
     public WebDriver webDriver;
+
     Logger logger = Logger.getLogger(getClass());
+
     private boolean isTestPass = false;
 
     public ParentTest() {
@@ -28,21 +29,26 @@ public class ParentTest {
         File driverPath = new File("../JavaMentor/drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", driverPath.getAbsolutePath());
 
-        webDriver = new ChromeDriver();
+        File driverPathEdge = new File("../JavaMentor/drivers/MicrosoftWebDriver.exe");
+        System.setProperty("webdriver.edge.driver", driverPathEdge.getAbsolutePath());
+
+       //webDriver = new ChromeDriver();
+        webDriver = new EdgeDriver();
 
         mainUrl = new MainUrl(webDriver);
 
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
+
     }
 
-        @After
-    public void tearDown() {
-        if (!(webDriver == null)) {
-            webDriver.quit();
-        }
-    }
+    //    @After
+//    public void tearDown() {
+//        if (!(webDriver == null)) {
+//            webDriver.quit();
+//        }
+//    }
     public void checkAC(String messageICantSee, boolean actualResult, boolean expectedResult) {
         logger.info("2.3 - " + messageICantSee + " in Parent Test");
         if (!(actualResult == expectedResult))
