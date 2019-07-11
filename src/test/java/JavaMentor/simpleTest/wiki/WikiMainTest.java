4c7cd7108b7f5e559ce4c183c2fd407073004559
Olena_Oliinyk <elenikqa@ukr.net>
2019-07-11 16:30:57 +0300

    page 83 page 101

--- src/test/java/JavaMentor/simpleTest/wiki/WikiMainTest.java
+++ src/test/java/JavaMentor/simpleTest/wiki/WikiMainTest.java
@@ -3,18 +3,28 @@ package JavaMentor.simpleTest.wiki;
 import org.junit.Test;
 
 public class WikiMainTest extends WikiMainPage {
+
     public WikiMainTest() {
 
     }
 
-
+    //page 83
     @Test
     public void selectRadioButton() {
         openTools();
         selectRadio();
         checkByAttributeValue();
         checkByCssSelector();
+        takeScreenshot();
 
+    }
 
+    //page 101
+    @Test
+    public void screenshotsWiki() {
+        openWiki();
+        takeScreenshot();
+        mouseDown();
+        takeScreenshot();
     }
 }
