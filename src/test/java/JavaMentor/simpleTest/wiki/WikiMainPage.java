6a283e7cc51fe1553f0a33fdbee680d220d5e978
Olena_Oliinyk <elenikqa@ukr.net>
2019-07-11 16:32:09 +0300

    7 11 2019

--- src/test/java/JavaMentor/simpleTest/wiki/WikiMainPage.java
+++ src/test/java/JavaMentor/simpleTest/wiki/WikiMainPage.java
@@ -1,25 +1,24 @@
 package JavaMentor.simpleTest.wiki;
 //page 83
+//page 101
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class WikiMainPage {
 
-
     private WebDriver webDriver;
     private Logger logger = Logger.getLogger(getClass());
-    private WebDriverWait webDriverWait15;
-
 
     @Before
     public void setUp() {
@@ -39,13 +38,6 @@ public class WikiMainPage {
 
     @After
     public void tearDown() {
-//        File scrFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-//        try {
-//            FileUtils.copyFile(scrFile, new File("../JavaMentor/screen" + System.currentTimeMillis() + ".png"));
-//        } catch (IOException e) {
-//            logger.info(e);
-//        }
-
         webDriver.quit();
 
     }
@@ -53,10 +45,13 @@ public class WikiMainPage {
     public void openTools() {
 
         webDriver.get("https://www.toolsqa.com/automation-practice-form/");
-
         logger.info("1 - Url opened");
     }
 
+    public void openWiki() {
+        webDriver.get("https://en.wikipedia.org/wiki/Main_Page");
+        logger.info("1 - Url opened");
+    }
 
     public void selectRadio() {
         WebElement oRadioButton = webDriver.findElement(By.id("sex-0"));
@@ -73,11 +68,29 @@ public class WikiMainPage {
         element.click();
 
     }
-    public void checkByCssSelector(){
+
+    public void checkByCssSelector() {
         WebElement element = webDriver.findElement(By.cssSelector("input[id='tool-2']"));
         String cssValue = element.getCssValue("cssSelector");
-        logger.info(cssValue+" css value");
+        logger.info(cssValue + " css value");
         element.click();
     }
+
+    public void takeScreenshot() {
+        File scrFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(scrFile, new File("../JavaMentor/screen" + System.currentTimeMillis() + ".png"));
+        } catch (IOException e) {
+            logger.info(e);
+        }
+    }
+
+    public void mouseDown() {
+        WebElement element = webDriver.findElement(By.id("mp-dyk"));
+        Actions actions = new Actions(webDriver);
+        actions.moveToElement(element);
+        actions.perform();
+    }
+
 
 }
