96f7b58217d8c794f4fdec850d3dd2d4b092db03
Olena_Oliinyk <elenikqa@ukr.net>
2019-08-08 15:38:22 +0300

    webService 8/08/2

--- src/test/java/JavaMentor/api/TestingRestMethods.java
+++ src/test/java/JavaMentor/api/TestingRestMethods.java
@@ -25,9 +25,10 @@ public class TestingRestMethods {
     public void getInventory() {
         given().contentType("application/json")
                 .when().get("https://petstore.swagger.io/v2/store/inventory")
-                .then().statusCode(200).body("sold", equalTo(132))
-                .body("pending", equalTo(128))
-                .body("swimming", equalTo(14))
+                .then().statusCode(200)
+                .body("sold", equalTo(181))
+                .body("pending", equalTo(167))
+                .body("swimming", equalTo(1))
                 .log().all();
     }
 
@@ -38,5 +39,24 @@ public class TestingRestMethods {
                 .then().statusCode(200).body("id", equalTo(2)).log().all();
     }
 
+    @Test
+    // valid ID value >= 1 and <= 10. Other values will generated exceptions
+    public void getOrderByWrongId() {
+        given().contentType("application/json")
+                .when().get("https://petstore.swagger.io/v2/store/order/22")
+                .then().statusCode(404)
+                .body("type", equalTo("error"))
+                .body("message", equalTo("Order not found"))
+                .log().all();
+    }
+
+    @Test
+    //
+    public void getOrderByMissedParameter() {
+        given().contentType("application/json")
+                .when().get("https://petstore.swagger.io/v2/store/order/")
+                .then().statusCode(405)
+                .log().all();
+    }
 
 }
